8d3551d8b3649478d0e6fefd9e2752f552d427d6
GitArcher <archinux01@gmail.com>
2019-01-24 19:39:08 +0200

    Переосмысленны навигация и дизайн

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const [btnHome, btnExample, btnAbout] = document.querySelectorAll('header > div')
+//const [btnHome, btnExample, btnAbout] = document.querySelectorAll('header > div')
+const tabs = Array.from( document.querySelectorAll('header > div') )
+//const [home, examples, about] = document.querySelectorAll('main section')
+const sections = Array.from( document.querySelectorAll('main section') )
+
 const main = document.querySelector('main')
 
-btnHome.addEventListener( 'click', moveMain(0) )
-btnExample.addEventListener( 'click', moveMain(1) )
-btnAbout.addEventListener( 'click', moveMain(2) )
+// btnHome.addEventListener( 'click', moveMain(3) )
+// btnExample.addEventListener( 'click', moveMain(2) )
+// btnAbout.addEventListener( 'click', moveMain(1) )
+//
+
+tabs.forEach( (tab, num) => {
+  tab.addEventListener( 'click', toggleActiveTab )
+  tab.addEventListener( 'click', moveMain(num) )
+})
 
 function moveMain(step) {
-  return () => main.style.transform = `translateY(${-step*100}%)`
+  return event => main.style.transform = `translateX(${ -(step/3)*100 }%)`
+}
+
+function toggleActiveTab() {
+  document.querySelector('.active').classList.remove('active')
+  this.classList.add('active')
+}
+
+function viewSection() {
+  sections
 }
